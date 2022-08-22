0f930cbd34e8178922a39b78ba8d7b4458361e5b
Elmira Ilyasova <capricorn_26@mail.ru>
2022-08-22 14:19:39 +0700

    Config bundle and add basic structure

---
+++ src/index.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+import * as ReactDOMClient from "react-dom/client";
+import Test from "./components/test";
+
+import './index.scss';
+
+const root = ReactDOMClient.createRoot(document.getElementById('root') as HTMLDivElement);
+
+root.render(
+  <></>
+);
