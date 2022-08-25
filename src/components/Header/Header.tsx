c412b48b6fb1ca7a14e5f99c499d212fdb565055
Elmira Ilyasova <capricorn_26@mail.ru>
2022-08-25 14:59:57 +0700

    Correct tags and varibales in Header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,19 +1,21 @@
 import * as React from 'react';
 import { Link, useLocation } from 'react-router-dom';
-import './Header.scss';
+
+import { AppUrl } from '../../consts';
 
 import MainNav from './MainNav/MainNav';
 import User from './User/User';
+import './Header.scss';
 
 const Header = ():JSX.Element => {
   let path = useLocation().pathname;
 
   return (
-    <div className="header">
+    <header className="header">
       <div className="header__logo">
         {path === '/'
           ? <img className="header__logo-image" src={require('../../img/fluffy-logo.png')} alt="Логотип Fluffy" />
-          : <Link to='/'><img className="header__logo-image" src={require('../../img/fluffy-logo.png')} alt="Логотип Fluffy" /></Link>
+          : <Link to={AppUrl.MAIN}><img className="header__logo-image" src={require('../../img/fluffy-logo.png')} alt="Логотип Fluffy" /></Link>
         }        
       </div>
 
@@ -24,7 +26,7 @@ const Header = ():JSX.Element => {
       <div className="header__user">
         <User/>
       </div>
-    </div>
+    </header>
   );
 };
 
